11537e07f751b76e041b04cddee71714fb0c846a
Yapo:Rubn Torres <ruben@schibsted.cl>
2014-05-22 16:33:39 -0400

    Initial commit, empty template for plugin

---
+++ js/jquery.dirrty.js
@@ -0,0 +1,33 @@
+/* 
+ * Dirrty v0.1.1
+ * jquery plugin to detect if the fields of a form had been modified
+ * (c)2014 Rubén Torres - rubentdlh@gmail.com
+ * Released under the MIT license
+ */
+
+ (function($) {
+
+ 	function Dirrty(preview, options){
+ 		this.previewElement=preview;
+ 		
+ 	}
+
+ 	Dirrty.prototype = {
+
+ 		activate: function(){
+ 			
+ 		}
+ 		
+ 	}
+
+ 	$.fn.dirrty = function(options) {
+ 		this.each(function(){
+ 			
+ 		});	
+ 	}
+
+ 	$.fn.gifplayer.defaults = {
+ 		label: 'gif'
+ 	};
+
+ })(jQuery);
