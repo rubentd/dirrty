173f4c0086e01039d040ca83788b161cc4c0c2e7
Yapo:Rubn Torres <ruben@schibsted.cl>
2014-05-28 10:50:57 -0400

    Added a method to verify if it's dirty at a given time

--- js/jquery.dirrty.js
+++ js/jquery.dirrty.js
@@ -6,13 +6,28 @@
  * Released under the MIT license
  */
 
- (function($) {
+//Save dirrty instances
+var singleDs = [];
+
+(function($) {
+
+ 	function getSingleton(id){
+ 		var result;
+ 		$(singleDs).each(function(){
+			if($(this)[0].id == id){ 
+				result = $(this)[0];
+			}
+ 		});
+ 		return result;
+ 	}
 
  	function Dirrty(form, options){
  		this.form=form;
  		this.isDirty=false;
  		this.options=options;
  		this.history = ["clean", "clean"]; //Keep track of last statuses
+ 		this.id=$(form).attr("id");
+ 		singleDs.push(this);
  	}
 
  	Dirrty.prototype = {
@@ -129,11 +144,29 @@
  	}
 
  	$.fn.dirrty = function(options) {
- 		this.each(function(){
- 			options = $.extend({}, $.fn.dirrty.defaults, options);
- 			var dirrty = new Dirrty($(this), options);
- 			dirrty.init();
- 		});	
+
+		if (/^(isDirty)$/i.test(options)) {
+			//Check if we have an instance of dirrty for this form
+			var d = getSingleton($(this).attr("id"));
+
+			if(!d){
+				var d = new Dirrty($(this), options);
+				d.init();
+			}
+			switch(options){
+				case 'isDirty':
+					return d.isDirty; 
+					break;
+			}
+
+		}else if (typeof options == 'object' || !options) {
+			this.each(function(){
+				options = $.extend({}, $.fn.dirrty.defaults, options);
+ 				var dirrty = new Dirrty($(this), options);
+ 				dirrty.init();
+			});	
+		}
+
  	}
 
  	$.fn.dirrty.defaults = {
